d0c199706ee4d9c8c0c0ccf33bc11d68326fba84
hasanasadov <hasanaliasadov@gmail.com>
2024-09-21 16:02:00 +0400

    bugs spotted in header

--- src/Comp/header/index.jsx
+++ src/Comp/header/index.jsx
@@ -1,37 +1,47 @@
-// import { useLocation } from "react-router-dom";
 import { Link, NavLink } from "react-router-dom";
-import React from "react";
+import React, { useState } from "react";
 import { navItems } from "../../Constants";
-
-import Logo  from "../../../public/logo.jpg";
+import Logo from "../../../public/logo.jpg";
 
 export const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   return (
-    <div className="sticky top-0 z-20 w-full py-2 px-10 bg-primary flex justify-between">
+    <div className="sticky top-0 z-20 w-full py-2 px-10 bg-primary flex justify-between items-center">
       <div className="flex items-center gap-x-3">
-        <Link to="/" className="flex items-center gap-x-2" >
-        <img src={Logo} alt="logo" className="w-10 h-10" />
-        <h1 className="text-white text-2xl font-bold">React Games</h1>
+        <Link to="/" className="flex items-center gap-x-2">
+          <img src={Logo} alt="logo" className="w-10 h-10" />
+          <h1 className="text-white text-2xl font-bold">React Games</h1>
         </Link>
       </div>
-      <div className="flex  items-center gap-x-3">
-        {navItems.map((navItem, idx) => {
-          return (
-            <NavLink
-              key={idx}
-              to={navItem.path}
-              className={({ isActive }) =>
-                `text-white text-lg font-light uppercase ${
-                  isActive && "text-purple-900  font-extrabold"
-                }`
-              }
-            >
-              {navItem.title}
-            </NavLink>
-          );
-        })}
-
+      <div className="flex items-center gap-x-3">
+        <button
+          className="text-white text-2xl md:hidden"
+          onClick={toggleMenu}
+        >
+          &#9776;
+        </button>
+        <div className={`md:flex ${isMenuOpen ? "slide-in" : "slide-out"} md:block`}>
+          {navItems.map((navItem, idx) => {
+            return (
+              <NavLink
+                key={idx}
+                to={navItem.path}
+                className={({ isActive }) =>
+                  `text-white text-lg font-light uppercase ${
+                    isActive && "text-purple-900 font-extrabold"
+                  }`
+                }
+              >
+                {navItem.title}
+              </NavLink>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
